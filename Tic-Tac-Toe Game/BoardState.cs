4aef1a41e12bb7889fa0077e35138504b6df4c46
jklawing <90219148+jklawing@users.noreply.github.com>
2021-09-20 19:46:49 -0400

    Add files via upload

---
+++ Tic-Tac-Toe Game/BoardState.cs
@@ -0,0 +1,16 @@
+﻿namespace Tic_Tac_Toe_Game
+{
+    class BoardState
+    {
+        public char zeroZero { get; set; }
+        public char oneZero{ get; set; }
+        public char twoZero { get; set; }
+        public char zeroOne { get; set; }
+        public char oneOne { get; set; }
+        public char twoOne { get; set; }
+        public char zeroTwo { get; set; }
+        public char oneTwo { get; set; }
+        public char twoTwo { get; set; }
+        public bool player1Turn { get; set; }
+    }
+}
