145008324d05530cd069863d6572692cfffa2eee
Jeffrey Lawing <90219148+jklawing@users.noreply.github.com>
2021-11-10 13:17:28 -0500

    Add files via upload

---
+++ Tic-Tac-Toe Game/MarkType.cs
@@ -0,0 +1,21 @@
+﻿namespace Tic_Tac_Toe_Game
+{
+    /// <summary>
+    /// Type of value a cell in the game is currently
+    /// </summary>
+    public enum MarkType
+    {
+        /// <summary>
+        /// Cell hasn't been clicked yet
+        /// </summary>
+        Free,
+        /// <summary>
+        /// Cell is an O
+        /// </summary>
+        Oh,
+        /// <summary>
+        /// Cell is an X
+        /// </summary>
+        Ex
+    }
+}
